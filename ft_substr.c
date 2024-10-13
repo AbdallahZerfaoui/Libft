e21bcd9a4e7662f3d277b98c943a857472939413
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2024-10-13 15:25:53 +0200

    substr
    fixed

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: azerfaou <azerfaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 20:55:25 by azerfaou          #+#    #+#             */
-/*   Updated: 2024/10/11 12:44:29 by azerfaou         ###   ########.fr       */
+/*   Updated: 2024/10/13 15:23:08 by azerfaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,15 +42,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_calloc(1, sizeof(char)));
 	if ((start + len) > s_len)
 		len = s_len - start;
-	if ((start + len) <= s_len)
-	{
-		str_result = (char *)ft_calloc(len + 1, sizeof(char));
-		if (!str_result)
-			return (NULL);
-		str_result = ft_strncpy(str_result, s + start, len);
-	}
-	else
-		str_result = ft_strdup(s);
+	str_result = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!str_result)
+		return (NULL);
+	str_result = ft_strncpy(str_result, s + start, len);
 	return (str_result);
 }
 
